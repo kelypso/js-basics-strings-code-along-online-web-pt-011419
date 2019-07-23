caa98e6ea32c127ac69c4b170863a573700b2cdd
Kelsey Low <kklowlow@gmail.com>
2019-07-23 21:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
-const currentUser = ""
-const welcomeMessage = "Welcome to Flatbook!"
+const currentUser = "Grace Hopper";
+const welcomeMessage = "Welcome to Flatbook!";
